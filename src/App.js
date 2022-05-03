939220f581b7e452ea92cecd77b480f66e523ca9
jalvarez957 <99046055+jalvarez957@users.noreply.github.com>
2022-05-02 17:15:37 -0700

    added numbered buttons to insert values into grid from

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react'
+import React, {useState, useReducer} from 'react'
 
 let initial = [
   [-1, 5, -1, 9, -1, -1, -1, -1, -1],
@@ -13,37 +13,43 @@ let initial = [
   [-1, -1, -1, -1, 3, -1, -1, 7, -1]
 ]
 
+
 function App() {
+  const [, forceUpdate] = useReducer(x => x + 1, 0);
   const [sudokuArr, setSudokuArr] = useState(initial);
+  const [selected, setSelected] = useState(0)
 
   function getDeepCopy(arr) {
     return JSON.parse(JSON.stringify(arr))
   }
 
-  function onInputChange(e, row, col) {
-    var val = parseInt(e.target.value) || -1, grid = getDeepCopy(sudokuArr)
-    if ( val === -1 || val >= 1 && val <= 9) {
-      grid[row][col] = val
-    }
-    setSudokuArr(grid)
+  function onNumberButtonClick(val) {
+    sudokuArr[Math.floor(selected/9)][selected%9] = val
+    setSudokuArr(sudokuArr)
+    forceUpdate()
+  }
+
+  function onGridButtonClick(row, col) {
+    setSelected(9*row+col)
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h3>Sudoku</h3>
-        <table>
+        <div></div>
+        <table className='grid-table'>
           <tbody>
             {
               [0,1,2,3,4,5,6,7,8].map((row, rIndex) => {
-                return <tr key={rIndex} className={(row + 1) % 3 === 0 ? 'bBorder':''}>
+                return <tr key={rIndex} className={(row + 1) % 3 === 0 && row != 8 ? 'bBorder':'grid-cell'}>
                   {[0,1,2,3,4,5,6,7,8].map((col, cIndex) => {
-                    return <td key={rIndex+cIndex} className={(col + 1) % 3 === 0 ? 'rBorder':''}>
-                      <input onChange={(e)=>onInputChange(e,row,col)} 
+                    return <td key={rIndex+cIndex} className={(col + 1) % 3 === 0 && col != 8 ? 'rBorder':'grid-cell'}>
+                      <button onClick={()=>onGridButtonClick(row,col)} 
                       value={sudokuArr[row][col] === -1 ? '' : sudokuArr[row][col]} 
                       className='cell'
                       disabled={initial[row][col] !== -1}
-                      />
+                      >{sudokuArr[row][col] === -1 ? '' : sudokuArr[row][col]}</button>
                     </td>
                     })}
                   </tr>
@@ -51,6 +57,19 @@ function App() {
             }
           </tbody>
         </table>
+        <div>
+          <table className='number-buttons-table'>
+            {
+              [1,2,3,4,5,6,7,8,9].map((num, val) => {
+                return <td className='number-buttons-td' key={val}>
+                    <button key={val} value={num} className='number-buttons' 
+                    onClick={()=>onNumberButtonClick(num)}
+                    >{num}</button>
+                  </td>
+              })
+            }
+          </table>
+        </div>
       </header>
     </div>
   );
